f4d632711e511274cd2e1bda67ccdb6ca14460b9
Justin Fortunato <jfortunato25@gmail.com>
2014-07-17 13:19:57 -0400

    dynamic autoload location so we can use parent projects autoloader

--- phpunit.php
+++ phpunit.php
@@ -1,3 +1,9 @@
 <?php
 
-require_once __DIR__ . '/vendor/autoload.php';
+// check if we are running tests as standalone project or if we are developing
+// as part of a larger project
+$composer_autoload = is_file(__DIR__ . '/vendor/autoload.php') ?
+    __DIR__ . '/vendor/autoload.php':
+    __DIR__ . '/../../autoload.php';
+
+require_once $composer_autoload;
